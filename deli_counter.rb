b0c533c953d7b778bf007d3d64a0eb4d88dabc31
Michael McGovern <mmcgovern.wework@gmail.com>
2019-04-11 17:51:33 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,6 +2,7 @@
 katzDeliLine = []
 
 def line(katzDeliLine, name)
+  if katzDeliLine.empty
   "The line is currently empty."
   katzDeliLine << "#{name}"
   puts "Welcome #{name}. You are #{katzDeliLine.size} in line."
