2859a72bb4bd8c49c9cba6d72538781fb2e51db7
Michael McGovern <mmcgovern.wework@gmail.com>
2019-04-11 18:01:32 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,8 +6,8 @@ def line(katzDeliLine)
     puts "The line is currently empty."
   else
     current_line = "The line is currently: "
-    katzDeliLine.each_with_index do |index, name|
-      current_line << "#{index + 1}. #{name}." 
+    katzDeliLine.each_with_index(1) do |name, index|
+      current_line << "#{index}. #{name}." 
     end
     puts current_line
   end
